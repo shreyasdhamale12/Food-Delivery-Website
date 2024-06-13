b78c9f7d9d3d3f5b3646707222aea4cf95d76d1d
Shreyas <82096986+shreyasdhamale12@users.noreply.github.com>
2024-06-14 00:20:21 +0530

    Add files via upload

---
+++ Food Website/JAVA TEST/shreyasgym.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class class1{
+    public int a = 5;
+    protected int b = 10;
+    int c = 15;
+    private int d = 20;
+
+}
+
+
+public class shreyasgym{
+public static void main(String[] args) {
+ 
+    Testing t = new Testing();
+    System.out.println(t.a);
+    System.out.println(t.b);
+    System.out.println(t.c);
+    // System.out.println(t.d);
+
+}
+}
